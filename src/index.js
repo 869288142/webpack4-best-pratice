3a8e34e44b165b6e0db58f384355458b12f8296f
加把劲骑士 <869288142@qq.com>
2021-10-13 21:58:58 +0800

    chore: update demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-import './images/babel-api-test.png'
+import c from './images/babel-api-test.png'
 const chenjiang = 1
 // Promise.resolve(1)
 console.log(1);
 import './css/global.scss'
 import('./test.vue').then(console.log)
-var var
